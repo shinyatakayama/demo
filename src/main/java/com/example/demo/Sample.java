0c96dd3178fdea9060c3264f23449a59929d4a57
shinyatakayama <snytkym7@gmail.com>
2020-08-25 23:55:48 +0900

    Study

---
+++ src/main/java/com/example/demo/Sample.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+@FunctionalInterface
+public interface Sample {
+	void sayHi();
+}
---
+++ src/main/java/com/example/demo/StreamSample.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamSample {
+	List<String> list = Arrays.asList("Apple", "Orange", "Melon");
+	list.stream.forEach(System.out::println);
+}
