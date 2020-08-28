89d5617851a32a46653687384bddab4b42a48c55
shinyatakayama <snytkym7@gmail.com>
2020-08-28 23:17:39 +0900

    Study

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
 interface InterfaceTest {
 	// 抽象メソッド
 	public String method(String name, int n);
@@ -14,6 +17,14 @@ public class Main {
 			return "Hello " + name + n + "!";
 		};
 		System.out.println(it.method("Java", 8));
+		
+        Integer[] num = {1, 2, 3, 4, 5};
+        List<Integer> l = Arrays.asList(num);
+ 
+        // ラムダ式の記述
+        l.forEach(System.out::println);
 	}
+	
+	
 
 }
