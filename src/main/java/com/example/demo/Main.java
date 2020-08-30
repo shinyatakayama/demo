116ca34d5b2f9e83f85bc3328cee0cc2bf69c37f
shinyatakayama <snytkym7@gmail.com>
2020-08-30 23:32:36 +0900

    Study

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 interface InterfaceTest {
 	// 抽象メソッド
@@ -18,20 +21,32 @@ public class Main {
 			return "Hello " + name + n + "!";
 		};
 		System.out.println(it.method("Java", 8));
-		
-        Integer[] num = {1, 2, 3, 4, 5};
-        List<Integer> l = Arrays.asList(num);
- 
-        // ラムダ式の記述
-        l.forEach(System.out::println);
-        
-    	List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
-    	integerList.stream().map(i -> "要素は" + i + "です").forEach(i -> System.out.println(i));
 
-    	List<Integer> integerList1 = Arrays.asList(1, 2, 3, 4, 5);
-    	integerList1.stream()
-    	        .sorted(Comparator.naturalOrder())
-    	        .forEach(i -> System.out.println(i));
+		Integer[] num = { 1, 2, 3, 4, 5 };
+		List<Integer> l = Arrays.asList(num);
+
+		// ラムダ式の記述
+		l.forEach(System.out::println);
+
+		List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
+		integerList.stream().map(i -> "要素は" + i + "です").forEach(i -> System.out.println(i));
+
+		List<Integer> integerList1 = Arrays.asList(1, 2, 3, 4, 5);
+		integerList1.stream().sorted(Comparator.naturalOrder()).forEach(i -> System.out.println(i));
+
+		String key = "3";
+		// nullの場合は空文字を指定するため、実行結果は0
+		System.out.println(getName(key).orElse("").length());
+
 	}
-	
+
+	private static Optional<String> getName(String key) {
+		Map<String, String> dataMap = new HashMap<>();
+		dataMap.put("1", "suzy");
+		dataMap.put("2", "michiya");
+		dataMap.put("3", "saya");
+
+		return Optional.ofNullable(dataMap.get(key));
+	}
+
 }
