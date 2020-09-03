7e5e919718e9260368ae4e645117fbb1a069ee1b
shinyatakayama <snytkym7@gmail.com>
2020-09-03 23:01:31 +0900

    Study

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 interface InterfaceTest {
@@ -39,26 +41,32 @@ public class Main {
 		// nullの場合は空文字を指定するため、実行結果は0
 		System.out.println(getName(key).orElse("").length());
 
-		
 		List<String> list = Arrays.asList("C", "C++", "Java", "Scala", "Ruby");
-		long count = list.stream()
-	            .filter(s -> s.startsWith("C"))
-	            .mapToInt(s -> s.length())
-	            .sum();
-		
+		long count = list.stream().filter(s -> s.startsWith("C")).mapToInt(s -> s.length()).sum();
+
 		System.out.println(count);
-		
-//		List<Integer> numbers = Arrays.asList(3, 1, -4, 1, -5, 9, -2, 6, 5, 3, 5);
-//		List<Integer> result = numbers.stream()
-//		        .filter(number -> number > 0)
-//		        .collect(Collectors.toList());
-//		System.out.println(result);
-		
+
+		List<Integer> numbers = Arrays.asList(3, 1, -4, 1, -5, 9, -2, 6, 5, 3, 5);
+		List<Integer> result = numbers.stream().filter(number -> number > 0).collect(Collectors.toList());
+		System.out.println(result);
+
 		List<String> strs = Arrays.asList("hoge", "fuga", "bars");
 
 		// ラムダ式
 		boolean b = strs.stream().anyMatch(s -> s.equals("fuga"));
 		System.out.println(b);
+
+		Set<String> set = new HashSet<String>() {
+			{
+				add("Windows");
+				add("Linux");
+				add("macOS");
+			}
+		};
+
+		for (String str : set) {
+			System.out.println(str); // 結果：Linux、Windows、macOS
+		}
 	}
 
 	private static Optional<String> getName(String key) {
@@ -69,7 +77,5 @@ public class Main {
 
 		return Optional.ofNullable(dataMap.get(key));
 	}
-	
-
 
 }
