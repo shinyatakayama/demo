5aaa7683076cc9157c730b9625b7799ba6db4c61
shinyatakayama <snytkym7@gmail.com>
2020-09-13 22:47:33 +0900

    Study

--- src/main/java/com/example/demo/Sample.java
+++ src/main/java/com/example/demo/Sample.java
@@ -1,8 +1,7 @@
 package com.example.demo;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Sample {
-
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
 }
