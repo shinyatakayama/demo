3a4902a24757feafad7b61a1110f2a213141755a
shinyatakayama <snytkym7@gmail.com>
2020-09-14 23:13:14 +0900

    Study

--- src/main/java/com/example/demo/Sample.java
+++ src/main/java/com/example/demo/Sample.java
@@ -1,7 +1,17 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("test");
+
+		final List<String> numTextList = Arrays.asList("0", "1", null);
+
+		final List<Integer> numList = numTextList.stream().filter(Objects::nonNull).map(Integer::parseInt)
+				.collect(Collectors.toList());
 	}
 }
