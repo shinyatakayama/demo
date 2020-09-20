62201545c13816df0101033433dc9db64079031d
shinyatakayama <snytkym7@gmail.com>
2020-09-20 22:53:38 +0900

    Study

--- src/main/java/com/example/demo/Sample.java
+++ src/main/java/com/example/demo/Sample.java
@@ -32,7 +32,7 @@ public class Sample {
 		System.out.println(filterList);
 		List<Employee> employees = new ArrayList<Employee>();
 		Employee employee = new Employee("john", 25, 3000.0, 9922001);
-		Employee employee2 = new Employee("sohn", 25, 3000.0, 9922001);
+		Employee employee2 = new Employee("sohn", 25, 2000.0, 9922001);
 		Employee employee3 = new Employee("aohn", 25, 3000.0, 9922001);
 		employees.add(employee);
 		employees.add(employee2);
@@ -44,7 +44,7 @@ public class Sample {
 	    Arrays.sort(test2, comparator);
 	    List<Employee> idList = Arrays.asList(test2);
 	    
-	   idList.stream().forEach(string -> System.out.println(string.getName()));
+	   idList.stream().filter(salary -> salary.getSalary() < 3000).forEach(string -> System.out.println(string.getName()));
 
 		List<Item> list = Arrays.asList(new Item(9, "apple"), new Item(3, "lemon"), new Item(6, "peach"),
 				new Item(6, "banana"));
@@ -55,6 +55,8 @@ public class Sample {
 		
 		String ids[] = new String[]{"a1", "a2", "a3"};
 //		List<String> idList = Arrays.asList(ids);
+		
+		
 		
 		
 	}
