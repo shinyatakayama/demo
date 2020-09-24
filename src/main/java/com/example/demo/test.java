dac2b740e1f47521d390b40c3d17cdf295a7ad5c
shinyatakayama <snytkym7@gmail.com>
2020-09-24 23:44:13 +0900

    Study

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class test {
 	public static void main(String[] args) {
-		System.out.println("test");
+		List<Item> list = Arrays.asList(new Item(9, "apple"), new Item(3, "lemon"), new Item(6, "peach"),
+				new Item(6, "banana"));
+
+		list.stream().sorted(Comparator.comparing(Item::getId)).map(id -> id.getName()).forEach(i -> System.out.println(i));
 	}
+
 }
