5e888e462cbfa20fff3f5a5f9212459f657773f6
shinyatakayama <snytkym7@gmail.com>
2020-09-26 22:12:00 +0900

    Study

--- src/main/java/com/example/demo/ComparatorImplTest.java
+++ src/main/java/com/example/demo/ComparatorImplTest.java
@@ -4,7 +4,10 @@ import java.util.Comparator;
 
 public class ComparatorImplTest {
 	public static void main(String[] args) {
-		Comparator<String> c = (s1, s2) -> s1.compareTo(s2);
-		System.out.println(c.compare("ABC", "DEF"));
+		Comparator<Integer> c = (s1, s2) -> s1.compareTo(s2);
+		System.out.println(c.compare(4, 4));
+		
+		Comparator<String> d = (s1, s2) -> s1.compareTo(s2);
+        System.out.println(d.compare("ABC", "DEF"));
 	}
 }
