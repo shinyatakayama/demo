022ded34e8bcfb56bf7fd449c7a1a6b85e9b8eaf
shinyatakayama <snytkym7@gmail.com>
2020-10-02 23:34:03 +0900

    Study

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -3,20 +3,18 @@ package com.example.demo;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Test {
 	public static void main(String[] args) {
-		List<Item> list = Arrays.asList(new Item(9, "apple"), new Item(3, "lemon"), new Item(6, "peach"),
-				new Item(6, "banana"));
-
-		list.stream().sorted(Comparator.comparing(Item::getId)).map(id -> id.getName()).forEach(i -> System.out.println(i));
+		Predicate<String> predicate = new Predicate<String>() {
+		    public boolean test(String s) {
+		        return s.startsWith("A");
+		    }
+		};
 		
-		int i = 0;
-        Supplier<Integer> supplier = () -> i;
-	
+		
 	}
-	
-	
 
 }
