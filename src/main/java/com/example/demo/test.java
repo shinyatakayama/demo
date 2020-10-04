c320c5848cfa593f6dacbcf75f970c2eaf326020
shinyatakayama <snytkym7@gmail.com>
2020-10-04 21:47:31 +0900

    Study

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -7,6 +7,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Test {
 	public static void main(String[] args) {
@@ -30,6 +31,10 @@ public class Test {
 		
 		List<String> list = Arrays.asList("AA", "AB", "BC");
 		list.stream().filter(predicate).map(function).forEach(consumer);
+		
+		List<String> testList = Arrays.asList("AA","BB","CC");
+		List<String> test = testList.stream().filter(s -> s.equals("AA") || s.endsWith("C")).map(s -> s.toLowerCase()).collect(Collectors.toList());
+		test.forEach(s -> System.out.println(s));
 		
 	}
 
